fa5de90b22a7576d3bb6837ae56a3ce828ba78a5
PauloHSNeto <paulo.silva1989@hotmail.com>
2022-02-15 12:02:21 -0300

    new

--- Desafio 1/src/com/company/Main.java
+++ Desafio 1/src/com/company/Main.java
@@ -4,14 +4,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
+        String spaces ="";
         String character ="";
         for (int i =0; i<n; i++){
+
+            //contagem de espaços//
+            for (int j=0;j+i+1<n;j++){spaces+=" ";}
             character+="*";
-            System.out.println(character);
+            System.out.println(spaces+character);
+            //reset dos espaços
+            spaces ="";
         }
         scanner.close();
 
