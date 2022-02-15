fb8a3ac796f49dfda0752d5d264ffab2d7cb9ebd
PauloHSNeto <paulo.silva1989@hotmail.com>
2022-02-15 11:15:35 -0300

    new

--- Desafio 3/src/com/company/Main.java
+++ Desafio 3/src/com/company/Main.java
@@ -1,10 +1,28 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        String palavra = scanner.nextLine();
+        //checar o numero de letras duplicadas
+        for (int i = 0; i < palavra.length(); i++) {
+            for (int j = i+1; j < palavra.length(); j++){
+                if (palavra.charAt(j)==palavra.charAt(i)){count++;break;}
+            }
+        }
+        System.out.println(count);
 
-        System.out.println("HelloWorld");
+
+
     }
+
 }
+
+/*# Questão 03
+Duas palavras podem ser consideradas anagramas de si mesmas se as letras de uma palavra podem ser realocadas para formar
+ a outra palavra. Dada uma string qualquer, desenvolva um algoritmo que encontre o número de pares de substrings que são
+  anagramas.
+*/
